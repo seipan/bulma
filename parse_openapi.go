9d0a77e142b37c8cfd9bfcfcc5e0e7cf49f0bf82
yamasaki <yamasakipann0218@gmail.com>
2023-07-29 19:57:57 +0900

    feart

--- parse_openapi.go
+++ parse_openapi.go
@@ -1,1 +1,28 @@
 package bluma
+
+import (
+	"fmt"
+
+	"github.com/getkin/kin-openapi/openapi3"
+)
+
+type OpenAPI struct {
+	pass string
+}
+
+func NewOpenAPI(pass string) *OpenAPI {
+	return &OpenAPI{
+		pass: pass,
+	}
+}
+
+func (o *OpenAPI) Pass() string {
+	return o.pass
+}
+
+func (o *OpenAPI) Parse() {
+	doc, err := openapi3.NewLoader().LoadFromFile(o.Pass())
+	if err != nil {
+		fmt.Println(err)
+	}
+}
