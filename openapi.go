84f998d771d99c7423eb3d8190004cc675095cea
seipan <yamasakipann0218@gmail.com>
2023-08-13 05:07:18 +0900

    feat : return method

--- openapi.go
+++ openapi.go
@@ -8,7 +8,27 @@ type Method struct {
 	bodys  []*openapi3.SchemaRef
 }
 
+func (m *Method) Method() string {
+	return m.method
+}
+
+func (m *Method) Params() openapi3.Parameters {
+	return m.params
+}
+
+func (m *Method) Bodys() []*openapi3.SchemaRef {
+	return m.bodys
+}
+
 type Path struct {
 	path   string
 	method []Method
+}
+
+func (p *Path) Path() string {
+	return p.path
+}
+
+func (p *Path) Method(index int) Method {
+	return p.method[index]
 }
