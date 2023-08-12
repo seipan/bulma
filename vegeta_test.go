1d2ef10cb6f113ea64ea221efb575a906f278136
seipan <yamasakipann0218@gmail.com>
2023-08-13 02:03:35 +0900

    tests : vegeta

--- vegeta_test.go
+++ vegeta_test.go
@@ -3,5 +3,20 @@ package bluma
 import "testing"
 
 func TestAttack(t *testing.T) {
-
+	t.Run("Attack", func(t *testing.T) {
+		atk := Attacker{
+			path: Path{
+				path: "http://localhost:8080/health",
+				method: []Method{
+					{
+						method: "GET",
+					},
+				},
+			},
+			methodIndex: 0,
+			frequency:   10,
+			duration:    1,
+		}
+		atk.Attack()
+	})
 }
