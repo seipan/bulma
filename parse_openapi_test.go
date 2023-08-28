3bc18632a0ae914b2249d1808b31d26ab13412da
seipan <yamasakipann0218@gmail.com>
2023-08-29 02:16:29 +0900

    feat : parse test init

--- parse_openapi_test.go
+++ parse_openapi_test.go
@@ -1,1 +1,14 @@
 package bluma
+
+import (
+	"context"
+	"testing"
+)
+
+func TestParseOpenAPI(t *testing.T) {
+	t.Run("ParseOpenAPI", func(t *testing.T) {
+		t.Skip()
+		o := NewOpenAPI("testdata/openapi.yaml")
+		o.Parse(context.Background())
+	})
+}
