5d3c677bdee63a687c5d90da5fc95ad411b752a8
seipan <yamasakipann0218@gmail.com>
2023-09-03 15:54:55 +0900

    fix : test equal

--- parse_openapi_test.go
+++ parse_openapi_test.go
@@ -3,12 +3,17 @@ package bluma
 import (
 	"context"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestParseOpenAPI(t *testing.T) {
 	t.Run("ParseOpenAPI", func(t *testing.T) {
-		t.Skip()
-		o := NewOpenAPI("testdata/openapi.yaml")
-		o.Parse(context.Background())
+		o := NewOpenAPI("testdata/openapi.json")
+		res, err := o.Parse(context.Background())
+		assert.NoError(t, err)
+
+		assert.Equal(t, "GET", res[0].method[0].method)
+		assert.Equal(t, "/health", res[0].Path())
 	})
 }
