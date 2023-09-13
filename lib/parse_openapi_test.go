bdd5f58e9b89b5846893b0ca4dac43ab7975b05f
seipan <yamasakipann0218@gmail.com>
2023-09-14 02:32:08 +0900

    test fix

--- lib/parse_openapi_test.go
+++ lib/parse_openapi_test.go
@@ -9,11 +9,11 @@ import (
 
 func TestParseOpenAPI(t *testing.T) {
 	t.Run("ParseOpenAPI", func(t *testing.T) {
-		o := NewOpenAPI("testdata/openapi.json")
+		o := NewOpenAPI("../testdata/openapi.json")
 		res, err := o.Parse(context.Background())
 		assert.NoError(t, err)
 
 		assert.Equal(t, "GET", res[0].method[0].method)
-		assert.Equal(t, "/health", res[0].Path())
+		assert.Equal(t, "/pets", res[0].Path())
 	})
 }
