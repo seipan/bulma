dfe5d4e18de330eaa0821ac4028d88d930027a15
yamasaki <yamasakipann0218@gmail.com>
2023-09-22 04:21:53 +0900

    fix : attack endpoint

--- lib/attack.go
+++ lib/attack.go
@@ -23,35 +23,29 @@
 package lib
 
 import (
-	"fmt"
-	"os"
+	"log"
 	"time"
 
 	vegeta "github.com/tsenart/vegeta/lib"
 )
 
-func (atk *Attacker) Attack() {
+func (atk *Attacker) Attack() vegeta.Metrics {
 	target := vegeta.Target{
 		Method: atk.Path.method[atk.MethodIndex].method,
 		URL:    atk.Path.path,
 		Body:   atk.Body,
 		Header: atk.Header,
 	}
+	log.Println(target)
 	targeter := vegeta.NewStaticTargeter(target)
 	rate := vegeta.Rate{Freq: atk.Frequency, Per: time.Second}
 
 	attacker := vegeta.NewAttacker()
-
 	var metrics vegeta.Metrics
-	var results vegeta.Results
-	reporter := vegeta.NewJSONReporter(&metrics)
-
 	for res := range attacker.Attack(targeter, rate, atk.Duration, "Vegeta Load Testing") {
-		results.Add(res)
+		metrics.Add(res)
 	}
-	results.Close()
-	reporter.Report(os.Stdout)
+	metrics.Close()
 
-	fmt.Printf("Mean response time: %s\n", metrics.Latencies.Mean)
-	fmt.Printf("99th percentile response time: %s\n", metrics.Latencies.P99)
+	return metrics
 }
--- lib/attack_test.go
+++ lib/attack_test.go
@@ -24,13 +24,10 @@ package lib
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestAttack(t *testing.T) {
 	t.Run("Attack", func(t *testing.T) {
-		t.Skip()
 		atk := Attacker{
 			Path: Path{
 				path: "http://localhost:8080/health",
@@ -44,6 +41,6 @@ func TestAttack(t *testing.T) {
 			Frequency:   10,
 			Duration:    1,
 		}
-		assert.NotPanics(t, atk.Attack)
+		atk.Attack()
 	})
 }
