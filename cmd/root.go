96e858ae0b6f29941d9ee218443579f41b2cf903
yamasaki <yamasakipann0218@gmail.com>
2023-09-23 05:31:36 +0900

    remove : default comment

--- cmd/root.go
+++ cmd/root.go
@@ -29,14 +29,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "bluma",
 	Short: "CLI tool to parse OpenAPI and stress test each endpoint.",
 	Long:  `CLI tool to parse OpenAPI and stress test each endpoint..`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 	Run: func(cmd *cobra.Command, args []string) {
 		path, err := cmd.Flags().GetString("path")
 		if err != nil {
@@ -61,8 +57,6 @@ var rootCmd = &cobra.Command{
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -71,14 +65,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.bluma.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().StringP("path", "p", "", "FilePath for Parsing OpenAPI")
 	rootCmd.Flags().StringP("base", "b", "", "BaseURL for stress test")
 	rootCmd.Flags().IntP("frequency", "f", 1, "stress test frequency")
