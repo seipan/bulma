d90a5520033e7230206c29bdf6934a284985f9af
seipan <yamasakipann0218@gmail.com>
2024-06-23 03:18:21 +0900

    feat : Igonore default option

--- lib/ignore.go
+++ lib/ignore.go
@@ -1,1 +1,24 @@
 package lib
+
+type Ignore struct {
+	Paths []string
+}
+
+func (i *Ignore) IsIgnored(path string) bool {
+	for _, p := range i.Paths {
+		if p == path {
+			return true
+		}
+	}
+	return false
+}
+
+func (i *Ignore) Add(path string) {
+	i.Paths = append(i.Paths, path)
+}
+
+func NewIgnore(paths []string) *Ignore {
+	return &Ignore{
+		Paths: paths,
+	}
+}
